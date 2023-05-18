bacb3a192e3b4596707daa58149dc19357d3bfd0
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-19 00:52:51 +0330

    ♻️ Refactor device entity.

--- src/Domain/Entities/Device.cs
+++ src/Domain/Entities/Device.cs
@@ -4,6 +4,7 @@ public class Device
 {
     Device() { }
 
-    public string Id { get; private set; }
+    public string Id { get; private set; } = Guid.Empty.ToString();
     public int Pin { get; private set; }
+    public bool IsHigh { get; set; } = false;
 }
