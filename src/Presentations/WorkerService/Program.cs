61944ebf9084e777055a12d1262feec56ba0a179
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-18 21:17:32 +0330

    🚀 Initialize worker service presentation.

--- src/Presentations/WorkerService/Program.cs
+++ src/Presentations/WorkerService/Program.cs
@@ -1,17 +1,20 @@
-namespace TechOnIt.WorkerService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            IHost host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices(services =>
-                {
-                    services.AddHostedService<Worker>();
-                })
-                .Build();
+using TechOnIt.Infrastructure.WebServices.Techonits;
 
-            host.Run();
-        }
+namespace TechOnIt.WorkerService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(async (hostContext, services) =>
+            {
+                services.AddCacheService(hostContext.Configuration);
+                services.AddTechonitService();
+                services.AddHostedService<Worker>();
+            })
+            .Build();
+
+        host.Run();
     }
 }
--- src/Presentations/WorkerService/Worker.cs
+++ src/Presentations/WorkerService/Worker.cs
@@ -1,16 +1,23 @@
+using TechOnIt.Infrastructure.WebServices.Techonits;
+
 namespace TechOnIt.WorkerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly ITechonitWebService _techonitWebService;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger,
+            ITechonitWebService techonitWebService)
         {
             _logger = logger;
+            _techonitWebService = techonitWebService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await _techonitWebService.Auth.GetAccessTokenAsync("Ba94QfKlm9k1vR3u", "123456", stoppingToken);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
