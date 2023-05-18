778e507bec0c0b0a6201f4b1611886509e7d7ff7
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-05-18 21:26:54 +0330

    🌱 Add injection & refactor namespaces.

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechOnIt.Infrastructure;
+
+namespace TechOnIt.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBoardManagerServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add cache service.
+        services.AddCacheService(configuration);
+        return services;
+    }
+}
